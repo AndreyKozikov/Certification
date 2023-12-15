2552bf5318597933970caf9f8267236eb130361f
Andrey Kozikov <nwisdom@mail.ru>
2023-12-15 21:39:36 +0500

    Добавлена блок-схема и реализован ввод массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arrayInput(int n)
+{
+    string[] arr = new string[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+
+int count;
+string str;
+do
+{
+    Console.Write("Вводим массив с клавиатуры? (Y - да / N - нет): ");
+    str = Console.ReadLine();
+    Console.WriteLine(str);
+}
+while (str != "Y" && str != "N");
+if (str == "Y")
+{
+    do
+    {
+        Console.Write("Введите число элементов массива: ");
+        str = Console.ReadLine();
+    }
+    while (!int.TryParse(str, out count) || count <= 0);
+    string[] array = arrayInput(count);
+}
+else
+{
+    string[] array = new string[] { "Hello", "2", "world", ":-)" };
+}
